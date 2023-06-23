3aa65b1a5bbbedce51d46bccf2fe4df843badd13
Sofía Alvarado <alvaradosofiaH@outlook.com>
2023-06-22 22:05:37 -0600

    Adding Firebase to project

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://shopping-list-27442-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList")
+
+
 let groceryList = [];
 const addButton = document.getElementById("add-button");
 const inputField = document.getElementById("input-field");
 const ulEl = document.getElementById("ul-el");
 
 addButton.addEventListener("click", function (){
-    groceryList.push(inputField.value);
+    let inputValue = inputField.value;
+    push(shoppingListInDB, inputValue);
     //inputField.value = "";
-    console.log(groceryList)
-    renderList();
+    console.log(`${inputValue} added to database`)
+    //console.log(groceryList)
+    //renderList();
 })
 
 function renderList() {
