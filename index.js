f3bc2c969c2c1027e2cc03f56e59b9385a60faaa
Sofía Alvarado <alvaradosofiaH@outlook.com>
2023-06-26 15:54:11 -0600

    Removing items when double click and adding hover to elements

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://shopping-list-27442-default-rtdb.firebaseio.com/"
@@ -32,16 +32,40 @@ function(snapshot) {} as the second argument
 */
 
 onValue(shoppingListInDB, function(snapshot) {
-    let arrayList = Object.values(snapshot.val());
-    clearList();
-    for (let i = 0; i <arrayList.length; i++) {
-        // Challenge: Use the appendItemToShoppingListEl(itemValue) function inside of the for loop to append item to the shopping list element for each iteration.
-        renderList(arrayList[i])
+    if(snapshot.exists()) {
+        let arrayList = Object.entries(snapshot.val());
+        clearList();
+        for (let i = 0; i <arrayList.length; i++) {
+            let currentItem = arrayList[i];
+            let currentItemID = currentItem[0];
+            let currentItemValue = currentItem[1];
+            //console.log(currentItemValue)
+            renderList(currentItem);
+        }
+    } else {
+        //clearList();
+        ulEl.innerHTML = "No items here... yet"
     }
+    
+
 })
 
-function renderList(value) {
-    ulEl.innerHTML += `<li> ${value} </li>`;
+function renderList(item) {
+    //ulEl.innerHTML += `<li> ${value} </li>`;
+    let itemID = item[0];
+    let itemValue = item[1];
+
+    let newEl = document.createElement("li");
+    newEl.textContent = itemValue;
+
+    newEl.addEventListener ("dblclick", function() {
+        let listItemLocation = ref(database, `shoppingList/${itemID}`);
+        remove(listItemLocation);
+        console.log(itemValue + " deleted")
+    });
+
+
+    ulEl.append(newEl);
 }
 
 function clearList() {
